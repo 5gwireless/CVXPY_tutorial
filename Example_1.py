1b8c8150a56f4a13c713405173c7a678d1205e3e
Saad Mahboob <sma57@sfu.ca>
2019-11-15 16:01:54 -0800

    first commit

---
+++ Example_1.py
@@ -0,0 +1,25 @@
+# https://www.cvxpy.org/examples/index.html
+
+import cvxpy as cp
+import numpy
+
+# Problem data.
+m = 30
+n = 20
+numpy.random.seed(1)
+A = numpy.random.randn(m, n)
+b = numpy.random.randn(m)
+
+# Construct the problem.
+x = cp.Variable(n)
+objective = cp.Minimize(cp.sum_squares(A*x - b))
+constraints = [0 <= x, x <= 1]
+prob = cp.Problem(objective, constraints)
+
+# The optimal objective is returned by prob.solve().
+result = prob.solve()
+# The optimal value for x is stored in x.value.
+print(x.value)
+# The optimal Lagrange multiplier for a constraint
+# is stored in constraint.dual_value.
+print(constraints[0].dual_value)
